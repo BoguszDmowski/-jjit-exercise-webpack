6c3cb07a5b80b25740d29b31c9bc203b5638ea6b
Bogusz Dmowski <boguszdmowski@gmail.com>
2020-08-26 19:22:38 +0200

    Fifth commit (bug fixes)

--- index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import React from "./App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         filename: "script.js",
     },
+    resolve: {
+        extensions: [".js", ".jsx"]
+    },
     module: {
         rules: [
            {
