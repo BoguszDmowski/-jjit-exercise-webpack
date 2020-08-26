301c92a4b1f117236613d92fea2d8f357f049021
Bogusz Dmowski <58112552+BoguszDmowski@users.noreply.github.com>
2020-08-26 15:28:16 +0200

    Fourth commit (babel)

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "script.js",
+    },
+    module: {
+        rules: [
+           {
+               test: /\.jsx?$/,
+               loader: "babel-loader",
+               exclude: /node_modules/,
+               query: {
+                   presets: ["@babel/preset-env", "@babel/preset-react"],
+               },
+           }, 
+        ],
+    },
+};
